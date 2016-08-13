24d876e4abccd317dda50d0df130d463ba958337
secure diversITy <mail@se-di.de>
2016-08-13 22:29:26 +0200

    extend with a sleep and fixing overwriting log..

--- recovery/root/createsym.sh
+++ recovery/root/createsym.sh
@@ -1,4 +1,6 @@
 #!/sbin/sh
-rm -Rf /dev/block/bootdevice > /tmp/$0.log
-ln -vs /dev/block/platform/f9824900.sdhci /dev/block/bootdevice > /tmp/$0.log
-ls -la /dev/block/ |grep bootdevice >/tmp/$0.log
+echo "sleeping a while" > /tmp/$0.log
+sleep 5
+rm -Rf /dev/block/bootdevice >> /tmp/$0.log
+ln -vs /dev/block/platform/f9824900.sdhci /dev/block/bootdevice >> /tmp/$0.log
+ls -la /dev/block/ |grep bootdevice >>/tmp/$0.log
