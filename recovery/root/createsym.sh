44bed2059a6d17eb59ba8ae4a252b3d022526313
secure diversITy <mail@se-di.de>
2016-08-13 22:36:27 +0200

    adding a symlink for soc.0

--- recovery/root/createsym.sh
+++ recovery/root/createsym.sh
@@ -3,4 +3,5 @@ echo "sleeping a while" > /tmp/$0.log
 sleep 5
 rm -Rf /dev/block/bootdevice >> /tmp/$0.log
 ln -vs /dev/block/platform/f9824900.sdhci /dev/block/bootdevice >> /tmp/$0.log
+ln -vs /dev/block/platform /dev/block/platform/soc.0  >> /tmp/$0.log
 ls -la /dev/block/ |grep bootdevice >>/tmp/$0.log
