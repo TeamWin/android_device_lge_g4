5cbe77f1a13c7573a23359ab9bf393a6f42f175f
secure diversITy <mail@se-di.de>
2016-08-15 16:29:33 +0200

    fix: Symlink is created now correctly. This enables qseecomd to work properly and so decryption within twrp!

--- recovery/root/createsym.sh
+++ recovery/root/createsym.sh
@@ -1,7 +1,11 @@
 #!/sbin/sh
-echo "sleeping a while" > /tmp/$0.log
-sleep 5
+# workaround script by steadfaster to ensure we have the required symlink on bootdevice
+echo "Starting $0" > /tmp/$0.log
+echo "bootdevice check before sleep:">>/tmp/$0.log
+ls -la /dev/block/ |grep bootdevice >>/tmp/$0.log
+echo "bootdevice check after sleep:">>/tmp/$0.log
+ls -la /dev/block/ |grep bootdevice >>/tmp/$0.log
 rm -Rf /dev/block/bootdevice >> /tmp/$0.log
 ln -vs /dev/block/platform/f9824900.sdhci /dev/block/bootdevice >> /tmp/$0.log
-ln -vs /dev/block/platform /dev/block/platform/soc.0  >> /tmp/$0.log
 ls -la /dev/block/ |grep bootdevice >>/tmp/$0.log
+echo "$0 finished">>/tmp/$0.log
