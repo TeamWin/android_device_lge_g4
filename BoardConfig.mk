81eda6b4110d5772e217cd784f6b3167666cd77e
steadfasterX <steadfastX@boun.cr>
2017-12-06 18:14:09 +0100

    new: added strace for debug in twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,8 +74,12 @@ endif
 # HW CRYPTO
 TW_INCLUDE_CRYPTO := true
 TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
-TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# TWRP debug flags
+TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
+TARGET_RECOVERY_DEVICE_MODULES += strace debuggerd
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/strace $(TARGET_OUT_EXECUTABLES)/debuggerd64
 
 # SEPOLICY stuff
 # https://android.googlesource.com/platform/external/sepolicy/+/marshmallow-release/README
