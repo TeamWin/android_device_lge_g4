752497a5f2ede251b89a6b5c2bf84286c7d8d075
steadfasterX <steadfastX@boun.cr>
2018-01-02 13:22:24 +0100

    fix: time
    since a recent commit to the LOS g4-common tree the QCOM
    time daemon gets used like in STOCK based ROMs. This breaks
    the time in TWRP (again...sigh) as before no custom ROM used
    the proprietary time daemon.
    To ensure that this will be solved forever (hopefully) and for any
    type of ROMs out there this commit introduce a complete new
    type of time daemon detection:
    instead of using the ROM flavor detection the time daemon binary
    existence will be checked

--- recovery/root/sbin/readtimeprop.sh
+++ recovery/root/sbin/readtimeprop.sh
@@ -3,6 +3,8 @@
 
 LOG=/tmp/recovery.log
 DEBUG=0
+QCOMTIMED=/tempsys/bin/time_daemon
+SONYTIMED=/tempsys/bin/timekeep
 
 F_LOG(){
    MSG="$1"
@@ -23,11 +25,20 @@ mkdir /tempsys
 mount -t ext4 /dev/block/bootdevice/by-name/system /tempsys 2>&1 >> $LOG || mount -t f2fs /dev/block/bootdevice/by-name/system /tempsys 2>&1 >> $LOG
 F_LOG "$(mount | grep tempsys)"
 F_LOG "$(ls -la /tempsys/build.prop)"
-[ ! -r /tempsys/build.prop ] && F_ELOG "cannot determine installed OS! time will may not work properly.. falling back to STOCK.."
+[ ! -r /tempsys/build.prop ] && F_ELOG "cannot determine installed OS! time will may not work properly.. falling back to qcomtime.."
 [ $DEBUG -eq 1 ] && [ -r /tempsys/build.prop ] && F_LOG "your build entries in yours ROM build.prop: $(grep build /tempsys/build.prop)"
+
+unset ROMTYPE
+[ -f $QCOMTIMED ] && ROMTYPE=qcomtime
+[ -f $SONYTIMED ] && ROMTYPE=sony
+
 SYSPROP=$(grep "ro.build.flavor" /tempsys/build.prop|cut -d "=" -f 2)
-echo "$SYSPROP" | egrep -i '(aosp|aicp|lineage|cyanogenmod|^cm_|^omni_)' >> /dev/null
-if [ $? -eq 0 ];then ROMTYPE=custom; else ROMTYPE=qcomtime; fi
+echo "$SYSPROP" | egrep -i '(aosp|aoscp|aicp|lineage|cyanogenmod|^cm_|^omni_)' >> /dev/null
+if [ $? -eq 0 ];then PROPTYPE=sony; else PROPTYPE=qcomtime; fi
+
+# fallback if the regular detection fails
+[ -z $QCOMTIMED ] && [ -z $SONYTIMED ] && ROMTYPE=$PROPTYPE && F_ELOG "binary detection failed! using fallback.."
+
 F_LOG "system umount"
 umount /tempsys 2>&1 >> $LOG
 rm -Rf /tempsys
@@ -66,30 +77,30 @@ else
     # clean the kernel buffer to see only the time related stuff
     [ $DEBUG -eq 1 ] && dmesg -c >> /dev/null
      
-    # if we are on STOCK and detect the proprietary time_daemon file ats_2 we start the qcom time_daemon
+    # if we are on a qcomtime ROM and detect the proprietary time_daemon file ats_2 we start the qcom time_daemon
     # but when not we assume the open source timekeep daemon and starting that instead
     # OR:
     #    - /data/property/persist.sys.timeadjust (when switching from CM/AOSP/... to STOCK)
     if [ "$ROMTYPE" == "qcomtime" ];then
-   F_LOG "STOCK or STOCK based ROM!"
+        F_LOG "QCOM time based ROM!"
         F_LOG "if you feel this is an error you may have and unidentified custom ROM flavor installed!"
-   F_LOG "Paste this line in the TWRP thread: flavor = $SYSPROP"
-         if [ -r /data/time/ats_1 ]||[ -r /data/time/ats_2 ]||[ -r /data/system/time/ats_1 ]||[ -r /data/system/time/ats_2 ];then
-       # we are on STOCK so we do not need custom ROM time file
+        F_LOG "Paste this line in the TWRP thread: flavor = $SYSPROP"
+        if [ -r /data/time/ats_1 ]||[ -r /data/time/ats_2 ]||[ -r /data/system/time/ats_1 ]||[ -r /data/system/time/ats_2 ];then
+            # we are on STOCK so we do not need custom ROM time file
             [ -f /data/property/persist.sys.timeadjust ] && rm /data/property/persist.sys.timeadjust && F_LOG "We are on a $ROMTYPE ROM so deleted unneeded CUSTOM ROM file: /data/property/persist.sys.timeadjust"
             F_LOG "proprietary qcom time-file detected! Will start qcom time_daemon instead of timekeep!"
-       # trigger time_daemon
-       setprop twrp.timedaemon 1
-         else
-       F_ELOG "We expected $ROMTYPE ROM but proprietary qcom time files missing! Cannot set time!"
-       F_ELOG "$(ls -la /data/time/ /data/system/time/)"
-         fi
+            # trigger time_daemon
+            setprop twrp.timedaemon 1
+        else
+            F_ELOG "We expected $ROMTYPE ROM but proprietary qcom time files missing! Cannot set time!"
+            F_ELOG "$(ls -la /data/time/ /data/system/time/)"
+        fi
     else
-   # when coming from STOCK those are obsolete!
-   [ -f /data/time/ats_1 ]&& rm /data/time/ats_* && F_LOG "atsa1: We are on a $ROMTYPE ROM so deleted unneeded STOCK ROM file: /data/time/ats_*"
-   [ -f /data/time/ats_2 ]&& rm /data/time/ats_* && F_LOG "atsa2: We are on a $ROMTYPE ROM so deleted unneeded STOCK ROM file: /data/time/ats_*"
-   [ -f /data/system/time/ats_1 ] && rm /data/system/time/ats_* && F_LOG "atsb1: We are on a $ROMTYPE ROM so deleted unneeded STOCK ROM file: /data/system/time/ats_*"
-   [ -f /data/system/time/ats_2 ] && rm /data/system/time/ats_* && F_LOG "atsb2: We are on a $ROMTYPE ROM so deleted unneeded STOCK ROM file: /data/system/time/ats_*"
+        # when coming from STOCK those are obsolete!
+        [ -f /data/time/ats_1 ]&& rm /data/time/ats_* && F_LOG "atsa1: We are on a $ROMTYPE ROM so deleted unneeded qcomtime ROM file: /data/time/ats_*"
+        [ -f /data/time/ats_2 ]&& rm /data/time/ats_* && F_LOG "atsa2: We are on a $ROMTYPE ROM so deleted unneeded qcomtime ROM file: /data/time/ats_*"
+        [ -f /data/system/time/ats_1 ] && rm /data/system/time/ats_* && F_LOG "atsb1: We are on a $ROMTYPE ROM so deleted unneeded qcomtime ROM file: /data/system/time/ats_*"
+        [ -f /data/system/time/ats_2 ] && rm /data/system/time/ats_* && F_LOG "atsb2: We are on a $ROMTYPE ROM so deleted unneeded qcomtime ROM file: /data/system/time/ats_*"
         
         if [ -r /data/property/persist.sys.timeadjust ];then
             setprop persist.sys.timeadjust $(cat /data/property/persist.sys.timeadjust)
@@ -101,7 +112,7 @@ else
         fi
      fi
 fi
-[ "$ROMTYPE" == "custom" ] && F_LOG "timeadjust property: >$(getprop persist.sys.timeadjust)<"
+[ "$ROMTYPE" == "sony" ] && F_LOG "timeadjust property: >$(getprop persist.sys.timeadjust)<"
 [ $DEBUG -eq 1 ] && F_LOG "$(dmesg)"
 
 F_LOG "$0 finished"
