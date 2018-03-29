141b9c9a71a43e80bd10c506eb58998f95ad084c
steadfasterX <steadfastX@boun.cr>
2018-03-29 14:33:22 +0200

    fix: UsU'd devices were not properly detected

--- init/libinit_g4.cpp
+++ init/libinit_g4.cpp
@@ -125,7 +125,7 @@ void get_usu(void)
 
         token = strtok(line, " ");
         while (token) {
-            if (memcmp(token, CMDLINE_USU, CMDLINE_USU_LEN) == 0 and (strstr(product_model,"LGLS991") or strstr(product_model,"LG-UsU"))) {
+            if (memcmp(token, CMDLINE_USU, CMDLINE_USU_LEN) == 0 and (strstr(product_model,"LGLS991") or strstr(product_model,"LG-USU"))) {
                 strcpy(usu_detect, "UsU_unlocked"); // UsU found
                 get_usu_model();
                 return;
