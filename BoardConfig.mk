56eb6207e1df1d18d7d9c69d148092a76aa911b7
steadfasterX <steadfastX@boun.cr>
2018-04-16 12:30:59 +0200

    enable debug flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,17 +77,17 @@ TW_INCLUDE_CRYPTO := true
 TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 
 # TWRP debug flags
-#TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
-#TARGET_RECOVERY_DEVICE_MODULES += strace
-#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/strace
+TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
+TARGET_RECOVERY_DEVICE_MODULES += strace
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/strace
 
 # SEPOLICY stuff
 # https://android.googlesource.com/platform/external/sepolicy/+/marshmallow-release/README
 BOARD_SEPOLICY_DIRS += device/lge/g4/sepolicy
 
 # DEBUG (BOTH needed to enable logcat)
-#TWRP_INCLUDE_LOGCAT := true
-#TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
 
 # Unify all g4 models
 TARGET_UNIFIED_DEVICE := true
